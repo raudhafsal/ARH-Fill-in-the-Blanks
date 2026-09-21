efcd6919c5c585167e30efcccfc4093f4df94ca6
raudhafsal <raudhafsal@gmail.com>
2026-09-21 21:35:58 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|manifest.json|icons/|sw.js|workbox-).*)",
+    "/((?!_next/static|_next/image|favicon.ico|favicon-32.png|manifest.json|icons/|brand/|apple-touch-icon.png|sw.js|workbox-).*)",
   ],
 };
